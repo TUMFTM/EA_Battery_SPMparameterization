5ff6ad9784646193a0c6813ec4a681f758bbd91d
ADS\ga73fej <wassiliadis@ftm.mw.tum.de>
2022-07-08 14:27:08 +0200

    Updated sensitivity analysis in coherence with published results

--- sensitivity/analysis.m
+++ sensitivity/analysis.m
@@ -1,5 +1,5 @@
 %% Sensitivity analysis of parameter variations to anode potential reserve control
-%  Published in January, 2022 by Nikolaos Wassiliadis
+%  Published in November, 2021 by Nikolaos Wassiliadis
 
 addpath('./measurement');
 addpath('./parameter');
@@ -9,39 +9,18 @@ close all;
 clc;
      
 %% Load Datasets
-load('measurement/US18650VTC5A_6C_0deg');
+SOH = 0.9; % Add SOH variation
+SOC_start = 0.1;
+SOC_end = 0.8;
 
 % Get actual cell capacity
-data.Q    = abs(min(Dataset.AhStep));
+data.Q    = 2.5*SOH;
 
-% Get relevant measurement range
-tmp = find(strcmp(Dataset.Command, 'CalcOnce')==1, 1, 'first');
-t   = Dataset.Time(tmp:end) - Dataset.Time(tmp);
-U   = Dataset.U(tmp:end);
-I   = Dataset.I(tmp:end);
-AhStep = Dataset.AhStep(tmp:end);
- 
-% Realign time
-tmp = find(I~=0, 1, 'first')-1;
-t   = t(tmp:end) - t(tmp);
-U   = U(tmp:end);
-I   = I(tmp:end);
-AhStep = AhStep(tmp:end);
- 
-tmp = find(I~=0, 1, 'last');
-t   = t(1:tmp);
-U   = U(1:tmp);
-I   = I(1:tmp);
-AhStep = AhStep(1:tmp);
- 
-% Ensure sample frequency of 10Hz
-[t_uni, idx_uni] = unique(t*3600);
-tmp = 0:0.1:max(t_uni);
-t   = tmp;
-U   = interp1(t_uni, U(idx_uni), tmp);
-I   = interp1(t_uni, I(idx_uni), tmp);
-T2  = zeros(1,length(I));
-SOC = interp1(t_uni, AhStep(idx_uni), tmp)*100./data.Q;
+% Ensure sample frequency of 1Hz
+t   = 0:0.1:(data.Q*(SOC_end-SOC_start))/15*3600; % 15A
+I   = ones(1, length(t))*15;
+SOC = (I.*t/3600/data.Q+SOC_start)*100;
+T2  = ones(1, length(t))*20;
 
 %% Configuration for sensitivity analysis
 
@@ -72,10 +51,8 @@ pscale = ones(16,1); %
 
 %% Execute model
 
-for idx_sim = 1:length(data)
 % Simulate new parameterization
-[t_sim, U_sim_a] = spmet_fnc_halfcells(t, I, T2, data.Q, 20, 10, 5, 10, pscale);
-end
+[t_sim, U_sim_a, OCV_a] = spmet_fnc_halfcells(t, I, T2, data.Q, 20, 10, 5, 10, pscale);
 
 
 %% Visualize: Anode potential over SOC
@@ -86,11 +63,11 @@ grid on;
 plot(SOC', U_sim_a);
 xlabel('SOC in %')
 ylabel('Anode potential in V')
-xlim([0 80]);
+xlim([10 80]);
 ylim([0 0.1]);
 
 % Downsampled for article publication
-%close all;
+close all;
 figure;
 set(gcf,'color','w');
 hold on;
@@ -98,38 +75,56 @@ grid on;
 plot(SOC(1:10:end)', U_sim_a(1:10:end));
 xlabel('SOC in %')
 ylabel('Anode potential in V')
-xlim([0 80]);
+xlim([10 80]);
 ylim([0 0.1]);
-%matlab2tikz('potential.tex')
+matlab2tikz('potential.tex')
+
+% Downsampled for article publication
+close all;
+figure;
+set(gcf,'color','w');
+hold on;
+grid on;
+plot(SOC(1:10:end)', OCV_a(1:10:end));
+xlabel('SOC in %')
+ylabel('Anode potential in V')
+xlim([10 80]);
+ylim([0 0.1]);
+matlab2tikz('potential_OCV.tex')
+
 
 %% Execute model
 
 % Parameter variation reach and step size
-step = .1;
+step = .01;
 reach = .1;
 window = (1-reach):step:(1+reach);
 thres = 0.05; % 50mV
 
-figure;
-hold on;
 % Loop for data generation
-for i=1:1%length(pscale)
+for i=1:length(pscale)
+    disp(['Running ' num2str(i) 'th line of parameter vector'])
     pscale = ones(16,1);
     for j=1:length(window)
+        disp(['Sample ' num2str(j) ' out of ' num2str(length(window))])
         % Change pscale
         pscale(i)=window(j);
         % Simulate new parameterization
         [t_sim, U_sim_a] = spmet_fnc_halfcells(t, I, T2, data.Q, 20, 10, 5, 10, pscale);
         % Find undershoot
+        try
         ix = find(U_sim_a<thres, 1, 'first');
         x = [U_sim_a(ix-1); U_sim_a(ix)];
         y = [SOC(ix-1); SOC(ix)];
         xq = thres;
         SOC_ix = interp1(x,y,xq);
+        catch
+        xq = NaN;
+        SOC_ix = NaN;
+        end
         % Transfer results
         results_simulation{i,j} = [SOC', U_sim_a];
         results_threshold{i,j} = [window(j), SOC_ix];
-        plot(SOC',U_sim_a);
     end
 end
 
@@ -168,9 +163,9 @@ xlabel('Parameter change in %')
 ylabel('Charging limit position change in %')
 legend('Area',...
        'Particle radius (anode)',...
-       'Particle radius (cathode)',...
+       'Particle radius (cathode)',...       
        'Active material capacity (anode)',...
-       'Active material capacity (cathide)',...
+       'Active material capacity (cathode)',...        
        '\sigma (anode)',...
        '\sigma (cathode)',...
        'S (anode)',...
@@ -188,7 +183,7 @@ savefig('./results/sensitivity.fig');
 
 
 % Downsampled for article publication
-%close all;
+close all;
 figure;
 set(gcf,'color','w');
 hold on;
@@ -205,9 +200,9 @@ xlabel('Parameter change in %')
 ylabel('Charging limit position change in %')
 legend('Area',...
        'Particle radius (anode)',...
-       'Particle radius (cathode)',...
+       'Particle radius (cathode)',...       
        'Active material capacity (anode)',...
-       'Active material capacity (cathide)',...
+       'Active material capacity (cathode)',...        
        '\sigma (anode)',...
        '\sigma (cathode)',...
        'S (anode)',...
